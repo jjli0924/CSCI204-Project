49a7658b580e8facbb5dd0380b7e7eb2f9d6d7a6
jjli0924 <jjl028@bucknell.edu>
2016-11-28 01:22:31 -0500

    Add files via upload

--- ProjectGUI.py
+++ ProjectGUI.py
@@ -1,14 +1,31 @@
 import sys
 from tkinter import *
 
-def bs():
+def doNothing():
     pass
 
-docEnt = StringVar()
+def bs():
+    pass
 
 Gui1 = Tk()
 Gui1.geometry('1000x500')
 Gui1.title("CSCI 204 Final Project")
+
+menu = Menu(Gui1)
+Gui1.config(menu=menu)
+
+docMenu = Menu(menu)
+menu.add_cascade(label=" Documents'", menu = docMenu)
+docMenu.add_command(label = "Documents Window", command=doNothing)
+
+
+trainMenu = Menu(menu)
+menu.add_cascade(label="Train", menu=trainMenu)
+trainMenu.add_command(label="Train", command = doNothing)
+
+predictMenu = Menu(menu)
+menu.add_cascade(label="Predict", menu=predictMenu)
+predictMenu.add_command(label="Predit", command = doNothing)
 
 authorLabel = Label(Gui1,text='Jonathan Li and Katie Lunceford',fg='white',bg='black')
 authorLabel.pack()
@@ -16,10 +33,22 @@ authorLabel.pack()
 DescriptionLabel = Label(Gui1,text='This program implements machine learning techniques for the purpose of authorship attribution',fg='white',bg='black')
 DescriptionLabel.pack()
 
+labelFrame = LabelFrame(Gui1,text = "Title                                   Genre            Year            Author ")
+labelFrame.pack(fill= "both", expand = "yes")
+
+#The different documents will be inserted into the document as followed.
+left = Label(labelFrame,text = "fadsfksad")
+left.pack()
+
+docName = StringVar()
+docEntry = Entry(Gui1, textvariable= docName)
+docEntry.grid(sticky=W)
+docEntry.pack()
+
 AddDocumentButton = Button(Gui1,text='Add Document',command=bs)
+AddDocumentButton.grid(sticky=E)
 AddDocumentButton.pack()
 
-RemoveDocumentButton = Button(Gui1,text='Remove Document',command=bs)
-RemoveDocumentButton.pack()
+trainFrame = LabelFrame(Gui1,text = "TRAIN")
+trainFrame.pack(fill= "both", expand = "yes")
 
-docEntry = Entry(Gui1, textvariable = docEnt).pack()
